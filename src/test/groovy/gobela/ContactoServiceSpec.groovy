c90ceea49104a3d8407d7ff288c9623fa7833a57
dsantamaria18 <dsantamaria_lago@hotmail.com>
2018-03-19 23:54:36 +0100

    añadido filtrado de contactos por entidad

---
+++ src/test/groovy/gobela/ContactoServiceSpec.groovy
@@ -0,0 +1,18 @@
+package gobela
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class ContactoServiceSpec extends Specification implements ServiceUnitTest<ContactoService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
