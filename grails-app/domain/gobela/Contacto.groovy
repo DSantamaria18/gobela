8a6fa2241d14095a0db94051a610779a706a87f0
David Santamaria <dsantamaria_lago@hotmail.com>
2018-11-28 23:50:09 +0100

    Añadido campo 'ACTIVO' a Contacto

--- grails-app/domain/gobela/Contacto.groovy
+++ grails-app/domain/gobela/Contacto.groovy
@@ -5,6 +5,7 @@ class Contacto {
     String nombre
     String email
     String telefono
+    Boolean activo
 
     static belongsTo = [entidad: Entidad]
 
@@ -12,9 +13,10 @@ class Contacto {
         nombre(nullable: false, blank: false)
         email(nullable: true, email: true, blank: true)
         telefono(nullable: true, blank: true)
+        activo()
     }
 
     String toString(){
-        return nombre?.toUpperCase()
+        return (activo) ? nombre?.toUpperCase() : nombre?.toUpperCase() + ' [INACTIVO]'
     }
 }
