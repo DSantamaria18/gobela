18a2490fe4d79407bbeae4da2bd203b9ceba7076
David Santamaria <dsantamaria_lago@hotmail.com>
2019-03-01 11:35:43 +0100

    [GOB-82] Creadas funciones en el servicio para obtener la lista de contactos

--- grails-app/services/gobela/ContactoService.groovy
+++ grails-app/services/gobela/ContactoService.groovy
@@ -10,4 +10,21 @@ class ContactoService {
         Entidad entidad = Entidad.get(entidadId)
         return Contacto.findAllByEntidad(entidad)
     }
+
+    def getListaClubes() {
+        return Entidad.findAllByEs_club(true, [sort: ['nombreEntidad': 'asc']])
+    }
+
+    def getListaContactos(def listaEntidades) {
+        def listaContactos = []
+
+        listaEntidades.each{
+            def contactos = it.contactos
+            contactos.each{
+                listaContactos.add(it)
+            }
+        }
+
+        return listaContactos
+    }
 }
