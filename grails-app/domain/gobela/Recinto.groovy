71a61783002675690defb90a14b7d02533f40de3
David Santamaria <dsantamaria_lago@hotmail.com>
2022-05-27 19:29:33 +0200

    gobela-112 [MULTIKIROLA] Añadir dirección a Recintos (link Google Maps)

--- grails-app/domain/gobela/Recinto.groovy
+++ grails-app/domain/gobela/Recinto.groovy
@@ -3,9 +3,11 @@ package gobela
 class Recinto {
 
     String nombre
+    String localizacion
 
     static constraints = {
         nombre(nullable: false, blank: false)
+        localizacion(nullable: true, blank: true)
     }
 
     String toString() {
